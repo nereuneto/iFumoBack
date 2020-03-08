0c90b2c28265e0b54c9faf0c1ba2fe0a0ac878ae
Nereu Neto <nereuneto2@hotmail.com>
2020-03-08 15:26:04 -0300

    Criada a operação de instânciação da tabela Categoria

--- src/main/java/com/nereuneto/iFumo/IFumoApplication.java
+++ src/main/java/com/nereuneto/iFumo/IFumoApplication.java
@@ -1,13 +1,35 @@
 package com.nereuneto.iFumo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class IFumoApplication {
+import com.nereuneto.iFumo.domain.Categoria;
+import com.nereuneto.iFumo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class IFumoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IFumoApplication.class, args);
+		
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Carvão");
+		Categoria cat2 = new Categoria(null,"Essência");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
