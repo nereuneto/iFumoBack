b0e2671c88691552a91714e0b5ac97146515f4c2
Nereu Neto <nereuneto2@hotmail.com>
2020-03-08 13:50:47 -0300

    Backend iniciado

---
+++ src/main/java/com/nereuneto/iFumo/IFumoApplication.java
@@ -0,0 +1,13 @@
+package com.nereuneto.iFumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFumoApplication.class, args);
+	}
+
+}
